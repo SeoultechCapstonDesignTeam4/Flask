665ac56ac78ecc53cdafead71b668015d0b4febb
hchbae1001 <hchbae1001@gmail.com>
2023-09-10 10:05:54 +0900

    이미지 전처리, class수정 완료

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ class BasicConv2d(nn.Module):
 
 # MobileNetV1
 class MobileNet(nn.Module):
-    def __init__(self, width_multiplier, num_classes=11, init_weights=True):
+    def __init__(self, width_multiplier, num_classes=5, init_weights=True):
         super().__init__()
         self.init_weights=init_weights
         alpha = width_multiplier
@@ -120,16 +120,19 @@ class MobileNet(nn.Module):
                 nn.init.constant_(m.bias, 0)
 
 device = "cuda" if torch.cuda.is_available() else   "cpu"
-model = MobileNet(1, 2)
+model = MobileNet(1, 5)
+
 state_dict = torch.load('eye.pt', map_location=device)
 # 모델의 state_dict에서 linear 레이어에 해당하는 가중치와 편향을 가져옵니다.
-model_state_dict = model.state_dict()
-for key in list(state_dict.keys()):
-    if 'linear' in key:  # linear 레이어에 해당하는 키 찾기
-        new_key = key.replace('linear', 'linear')  # 현재 모델의 키로 변경
-        state_dict[new_key] = state_dict.pop(key)
+# model_state_dict = model.state_dict()
+# for key in list(state_dict.keys()):
+#     if 'linear' in key:  # linear 레이어에 해당하는 키 찾기
+#         new_key = key.replace('linear', 'linear')  # 현재 모델의 키로 변경
+#         state_dict[new_key] = state_dict.pop(key)
 # 모델에 적용
 model.load_state_dict(state_dict)
+model.eval()
+
 # model.load_state_dict(torch.load('eye.pt', map_location=device))
 # model.eval()
 # eye_check_model = MobileNet(1,2)
@@ -168,28 +171,53 @@ image_transforms = transforms.Compose([
     # predicted_class = torch.argmax(probabilities, dim=1)
     # confidence = torch.max(probabilities).item() * 100
     # print(f"Confidence: {confidence:.2f}%")
+def preprocess_image(image_path):
+    # 이미지를 열고 RGB 형식으로 변환
+    image = Image.open(image_path).convert("RGB")
+    
+    # 이미지 크기를 조정 (224x224로 설정)
+    image = transforms.Resize((224, 224))(image)
+    
+    # 이미지를 텐서로 변환
+    image_tensor = transforms.ToTensor()(image)
+    
+    # 이미지 텐서를 모델에 맞게 정규화 (예: ImageNet의 평균 및 표준 편차)
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    image_tensor = normalize(image_tensor)
+    
+    # 배치 차원을 추가하여 모델의 예상 입력 형식으로 조정
+    image_tensor = image_tensor.unsqueeze(0)
+    
+    return image_tensor
+
+eye_label = ['정상', '결막염', '백내장', '색소침착성 각막염', '유루증']
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    image_file = request.files['image']
-    image = Image.open(image_file)
-    image_tensor = image_transforms(image).unsqueeze(0)
+    try:
+        image_file = request.files['image']
+        
+        # 이미지 전처리 함수를 사용하여 이미지를 텐서로 변환
+        image_tensor = preprocess_image(image_file)
 
-    with torch.no_grad():
-        output = model(image_tensor)
+        with torch.no_grad():
+            output = model(image_tensor)
+        
+        probabilities = F.softmax(output, dim=1)
+        predicted_classes = torch.argsort(probabilities, descending=True)[0].tolist()
+        class_probabilities = [probabilities[0][i].item() * 100 for i in predicted_classes]
 
-    label = ['conjunctivitis', 'normal']
-    _, predicted = torch.max(output.data, 1)
-    # print('Predicted:', label[predicted.item()])
+        predicted_labels = []
+        for i in predicted_classes:
+            if 0 <= i < len(eye_label):
+                predicted_labels.append(eye_label[i])
+            else:
+                predicted_labels.append("Unknown")
 
-    # image.save('test.jpg')
-
-    probabilities = F.softmax(output, dim=1)
-    # predicted_class = torch.argmax(probabilities, dim=1)
-    confidence = torch.max(probabilities).item() * 100
-    # print(f"Confidence: {confidence:.2f}%")
-    data = {'Predicted': label[predicted.item()], 'Confidence': confidence}
-    return jsonify(data)
+        data = {'Predicted': predicted_labels, 'Confidence': class_probabilities}
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 
 if __name__ == '__main__':
